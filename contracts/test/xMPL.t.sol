cea103a9df0e8abada9b99ff0094feabe5281073
JG Carvalho <jg@jgcarv.com>
2022-02-24 09:58:00 -0300

    feat: added more assertions and another test case

--- contracts/test/xMPL.t.sol
+++ contracts/test/xMPL.t.sol
@@ -1,67 +1,123 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.7;
 
+import { CompromisedMigrator } from "./mocks/Mocks.sol";
+
 import { TestUtils } from "../../modules/contract-test-utils/contracts/test.sol";
 
 import { MockERC20 } from "../../modules/mpl-migration/modules/erc20/src/test/mocks/MockERC20.sol";
 import { Migrator }  from "../../modules/mpl-migration/contracts/Migrator.sol";
 
+import { Staker } from "../../modules/revenue-distribution-token/src/test/accounts/Staker.sol";
+
 import { xMPL } from "../xMPL.sol";
 
 import { xMPLOwner } from "./accounts/Owner.sol";
 
-import { CompromisedMigrator } from "./mocks/Mocks.sol";
 
 contract xMPLTest is TestUtils {
 
     uint256 constant OLD_SUPPLY = 10_000_000 ether;
+    uint256 constant DEPOSITED = 1 ether;
+
+    uint256 start;
 
     Migrator  migrator;
     MockERC20 newToken;
     MockERC20 oldToken;
+    Staker    staker;
     xMPL      xmpl;
     xMPLOwner notOwner;
     xMPLOwner owner;
 
     function setUp() external {
+        // Use non-zero timestamp
+        start = 10_000;
+        vm.warp(start);
+
         oldToken = new MockERC20("Old Token", "OT", 18);
         newToken = new MockERC20("New Token", "NT", 18);
 
         migrator = new Migrator(address(oldToken), address(newToken));
 
-        newToken.mint(address(migrator), OLD_SUPPLY);
-
         owner    = new xMPLOwner();
         notOwner = new xMPLOwner();
 
+        staker = new Staker();
+
+        newToken.mint(address(migrator), OLD_SUPPLY);
+        oldToken.mint(address(staker), DEPOSITED);
+
         xmpl = new xMPL("xMPL", "xMPL", address(owner), address(oldToken), 1e30);
+
+        staker.erc20_approve(address(oldToken), address(xmpl), DEPOSITED);
+        staker.rdToken_deposit(address(xmpl), DEPOSITED);
     }
 
     function test_migrateAll_migration(uint256 amount_, uint vestingPeriod_) external {
-        amount_        = constrictToRange(amount_,                 1, OLD_SUPPLY);
+        amount_        = constrictToRange(amount_,        1,          OLD_SUPPLY - DEPOSITED);
         vestingPeriod_ = constrictToRange(vestingPeriod_, 10 seconds, 100_000 days) / 10 * 10;
+
         oldToken.mint(address(xmpl), amount_);
         owner.rdToken_updateVestingSchedule(address(xmpl), vestingPeriod_);
 
-        assertEq(oldToken.balanceOf(address(xmpl)),       amount_);
+        vm.warp(xmpl.vestingPeriodFinish());
+        
+        assertEq(oldToken.balanceOf(address(xmpl)),       amount_ + DEPOSITED);
         assertEq(newToken.balanceOf(address(xmpl)),       0);
         assertEq(xmpl.underlying(),                       address(oldToken));
         assertEq(xmpl.balanceOfUnderlying(address(this)), 0);
-        assertEq(xmpl.totalHoldings(),                    0);
-        assertEq(xmpl.exchangeRate(),                     1e30);
+
+        uint256 expectedRate     = amount_ * 1e30 / vestingPeriod_;
+        uint256 expectedHoldings = DEPOSITED + expectedRate * (block.timestamp - start) / 1e30;
+
+        assertEq(xmpl.totalHoldings(), expectedHoldings);
+        assertEq(xmpl.exchangeRate(),  expectedHoldings * 1e30 / DEPOSITED);
 
         owner.xMPL_migrateAll(address(xmpl), address(migrator), address(newToken));
 
         assertEq(oldToken.balanceOf(address(xmpl)),       0);
-        assertEq(newToken.balanceOf(address(xmpl)),       amount_);
+        assertEq(newToken.balanceOf(address(xmpl)),       amount_ + DEPOSITED);
         assertEq(xmpl.underlying(),                       address(newToken));
         assertEq(xmpl.balanceOfUnderlying(address(this)), 0);
-        assertEq(xmpl.totalHoldings(),                    0);
-        assertEq(xmpl.exchangeRate(),                     1e30);
+
+        assertEq(xmpl.totalHoldings(), expectedHoldings);
+        assertEq(xmpl.exchangeRate(),  expectedHoldings * 1e30 / DEPOSITED);
+    }
+
+    function test_migrateAll_migrationBeforVestingEnds(uint256 amount_, uint vestingPeriod_) external {
+        amount_        = constrictToRange(amount_,        1,          OLD_SUPPLY - DEPOSITED);
+        vestingPeriod_ = constrictToRange(vestingPeriod_, 10 seconds, 100_000 days) / 10 * 10;
+
+        oldToken.mint(address(xmpl), amount_);
+        owner.rdToken_updateVestingSchedule(address(xmpl), vestingPeriod_);
+
+        vm.warp(block.timestamp + vestingPeriod_ / 2);
+        
+        assertEq(oldToken.balanceOf(address(xmpl)),       amount_ + DEPOSITED);
+        assertEq(newToken.balanceOf(address(xmpl)),       0);
+        assertEq(xmpl.underlying(),                       address(oldToken));
+        assertEq(xmpl.balanceOfUnderlying(address(this)), 0);
+
+        uint256 expectedRate     = amount_ * 1e30 / vestingPeriod_;
+        uint256 expectedHoldings = DEPOSITED + expectedRate * (block.timestamp - start) / 1e30;
+
+        assertEq(xmpl.totalHoldings(), expectedHoldings);
+        assertEq(xmpl.exchangeRate(),  expectedHoldings * 1e30 / DEPOSITED);
+
+        owner.xMPL_migrateAll(address(xmpl), address(migrator), address(newToken));
+
+        assertEq(oldToken.balanceOf(address(xmpl)),       0);
+        assertEq(newToken.balanceOf(address(xmpl)),       amount_ + DEPOSITED);
+        assertEq(xmpl.underlying(),                       address(newToken));
+        assertEq(xmpl.balanceOfUnderlying(address(this)), 0);
+
+        assertEq(xmpl.totalHoldings(), expectedHoldings);
+        assertEq(xmpl.exchangeRate(),  expectedHoldings * 1e30 / DEPOSITED);
     }
 
     function test_migrateAll_failIfNotOwner(uint256 amount_) external {
-        amount_ = constrictToRange(amount_, 1, OLD_SUPPLY);
+        amount_ = constrictToRange(amount_, 1, OLD_SUPPLY - DEPOSITED);
         oldToken.mint(address(xmpl), amount_);
 
         vm.expectRevert("XMPL:MA:NOT_OWNER");
@@ -70,12 +126,12 @@ contract xMPLTest is TestUtils {
         owner.xMPL_migrateAll(address(xmpl), address(migrator), address(newToken));
 
         assertEq(oldToken.balanceOf(address(xmpl)), 0);
-        assertEq(newToken.balanceOf(address(xmpl)), amount_);
+        assertEq(newToken.balanceOf(address(xmpl)), amount_ + DEPOSITED);
         assertEq(xmpl.underlying(),                 address(newToken));
     }
 
     function test_migrateAll_failIfWrongToken(uint256 amount_) external {
-        amount_ = constrictToRange(amount_, 1, OLD_SUPPLY);
+        amount_ = constrictToRange(amount_, 1, OLD_SUPPLY - DEPOSITED);
         oldToken.mint(address(xmpl), amount_);
 
         vm.expectRevert("XMPL:MA:WRONG_TOKEN");
@@ -84,12 +140,12 @@ contract xMPLTest is TestUtils {
         owner.xMPL_migrateAll(address(xmpl), address(migrator), address(newToken));
 
         assertEq(oldToken.balanceOf(address(xmpl)), 0);
-        assertEq(newToken.balanceOf(address(xmpl)), amount_);
+        assertEq(newToken.balanceOf(address(xmpl)), amount_ + DEPOSITED);
         assertEq(xmpl.underlying(),                 address(newToken));
     }
 
     function test_migrateAll_failIfMismatchedBalance(uint256 amount_) external {
-        amount_ = constrictToRange(amount_, 1, OLD_SUPPLY);
+        amount_ = constrictToRange(amount_, 1, OLD_SUPPLY - DEPOSITED);
         oldToken.mint(address(xmpl), amount_);
 
         CompromisedMigrator migrator_ = new CompromisedMigrator(address(oldToken), address(newToken));
@@ -100,7 +156,7 @@ contract xMPLTest is TestUtils {
         owner.xMPL_migrateAll(address(xmpl), address(migrator), address(newToken));
 
         assertEq(oldToken.balanceOf(address(xmpl)), 0);
-        assertEq(newToken.balanceOf(address(xmpl)), amount_);
+        assertEq(newToken.balanceOf(address(xmpl)), amount_ + DEPOSITED);
         assertEq(xmpl.underlying(),                 address(newToken));
     }
 
