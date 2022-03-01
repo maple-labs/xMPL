3bd0b6900e07cf557ad6cc873627ea2b7b7d5762
Vedran Bidin <vedran@bidin.me>
2022-03-01 15:40:05 -0500

    ci: add foundry profile config

--- test.sh
+++ test.sh
@@ -1,22 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-while getopts t:r:b:v:c: flag
+while getopts p:m: flag
 do
     case "${flag}" in
-        t) test=${OPTARG};;
-        r) runs=${OPTARG};;
+        p) profile=${OPTARG};;
+        m) match=${OPTARG};;
     esac
 done
 
-runs=$([ -z "$runs" ] && echo "10" || echo "$runs")
+export FOUNDRY_PROFILE=$profile
 
-export DAPP_SOLC_VERSION=0.8.7
-export DAPP_SRC="contracts"
-export DAPP_LIB="modules"
-export PROPTEST_CASES=$runs
-export RUST_BACKTRACE=full
+if [ -z "$test" ]; then match="[src/test/*.t.sol]"; else match=$test; fi
 
-if [ -z "$test" ]; then match="[contracts/test/*.t.sol]"; else match=$test; fi
+echo Using profile: $FOUNDRY_PROFILE
 
-forge test --match "$match" -vvv --lib-paths "modules" --contracts "contracts" 
+forge test --match "$match" -vvv
