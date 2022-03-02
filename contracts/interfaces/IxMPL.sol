95a7f3135df7fa11790ffaa55ec62555c322189e
JG Carvalho <jg@jgcarv.com>
2022-03-02 17:28:09 -0300

    chore: updated natspec interface

--- contracts/interfaces/IxMPL.sol
+++ contracts/interfaces/IxMPL.sol
@@ -9,16 +9,32 @@ interface IxMPL is IRevenueDistributionToken {
     /*** Events ***/
     /**************/
 
+    /**
+    *  @dev Notifies that a scheduled migration was cancelled.
+    */
     event MigrationCancelled();
 
+    /**
+    *  @dev   Notifies that a scheduled migration was executed.
+    *  @param amount the amount of tokens migrated.
+    */
     event MigrationPerformed(uint256 amount);
 
+    /**
+    *  @dev   Notifies that migration was scheduled.
+    *  @param from     current asset address.
+    *  @param to       address of the asset to be migrated to.
+    *  @param migrator address of the migrator contract.
+    */
     event MigrationScheduled(address from, address to, address migrator);
 
     /********************************/
     /*** Administrative Functions ***/
     /********************************/
 
+    /**
+    *  @dev Cancel the scheduled migration
+    */
     function cancelMigration() external;
 
     /**
@@ -28,16 +44,33 @@ interface IxMPL is IRevenueDistributionToken {
     */
     function performMigration(address migrator_, address newAsset_) external;
 
-    function scheduleMigration(address migrator_, address newAsset) external;
+    /**
+    *  @dev   Schedule a migration to be executed after a delay.
+    *  @param migrator_ The address of the migrator contract.
+    *  @param newAsset_ The address of the new asset token.
+    */
+    function scheduleMigration(address migrator_, address newAsset_) external;
 
     /**********************/
     /*** View Functions ***/
     /**********************/
 
+    /**
+    *  @dev    Get the migration has of a scheduled migration.
+    *  @return migrationHash_ The hash of the migration parameters.
+    */
     function migrationHash() external view returns (bytes32 migrationHash_);
 
+    /**
+    *  @dev    Get the timestamp that a migration was scheduled.
+    *  @return migrationScheduled_ The timestamp of the migration.
+    */
     function migrationScheduled() external view returns (uint256 migrationScheduled_);
 
+    /**
+    *  @dev    Get the minimum delay that a schedule transactions needs to be executed.
+    *  @return minimumDelay_ The delay in seconds.
+    */
     function minimumDelay() external pure returns (uint256 minimumDelay_);
 
 }
